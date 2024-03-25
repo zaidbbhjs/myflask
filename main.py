a0db625100154f34269498dabd27edfd31528072
zaidbbhjs <151960814+zaidbbhjs@users.noreply.github.com>
2024-03-25 16:23:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,85 +1,111 @@
-import os,sys
-#os.system("pip install fastapi")
-#os.system("pip install uvicorn")
-from fastapi import Response
-import requests
-import os
-import random
-import uvicorn,time
-from fastapi import Request
-import json
-from user_agent import generate_user_agent
-from fastapi.responses import JSONResponse
-from fastapi import FastAPI
-import secrets
-app = FastAPI()
-import requests
-import requests , time,random
-from json import dumps
-r = requests.session()
-a=int(0)
-from fastapi import FastAPI, UploadFile
-from secrets import token_hex
-import uuid,secrets
-cok = token_hex(8) * 2
-uuid = str(uuid.uuid4())
-app = FastAPI()
-@app.get('/api/gmail/{gmail}')
-async def ga(gmail):
-    global cok,uuid
-    email=gmail
-    url = 'https://accounts.google.com/_/lookup/accountlookup?hl=ar&_reqid=32589&rt=j'
-	
-    headers = {
-		'accept': '*/*',
-		'accept-encoding': 'gzip, deflate, br',
-		'accept-language': 'ar,en-US;q=0.9,en;q=0.8',
-		'content-length': '3911',
-		'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
-		'cookie': 'OGPC=422038528-2:; SID=8Ae5CNXyYLku7h3Nhd6PmjEwsqpLep9sdfcDc_QeJT1m6pf_cFdWBefOdFWBrRatzQzoTw.; __Secure-3PSID=8Ae5CNXyYLku7h3Nhd6PmjEwsqpLep9sdfcDc_QeJT1m6pf_LWBgOEuV0LWH29_CLb3l1g.; HSID=A8KaTqMCOG6xpfvsz; SSID=AtYd81IgyZuE9EbfE; APISID=E3Psm5Uangi4fH9M/AkOnPYEUZWnD-tnA_; SAPISID=iN7Q0OqbHZcyy5FL/Aerh1_4xeYlLJY4Hq; __Secure-3PAPISID=iN7Q0OqbHZcyy5FL/Aerh1_4xeYlLJY4Hq; ACCOUNT_CHOOSER=AFx_qI6QfbFWoV6PV6XKN_T6BDu29QAEvMrEZsoAl1r4bDBfnWApbNKbPlRCbFUWBfZ_IufZlpgrXPJIQyLZtlrdzhTBLG1ugbS2CJ2q9HNMMkzfgeaXgctISpVwNdXddAWu4ZnL0x6TC4OCJGrmngsaE5GSmcovCQ; LSID=o.myaccount.google.com|s.youtube:8Ae5CGkVX0iajA0zvRf3mhX7pmhByOogOBptOhnbeOOuoJS6lsMzh7eIoJ7jRz_OOQU1DQ.; SEARCH_SAMESITE=CgQImZIB; __Host-3PLSID=doritos|o.myaccount.google.com|s.youtube:8Ae5CGkVX0iajA0zvRf3mhX7pmhByOogOBptOhnbeOOuoJS6ByLMNjL9oNzA97kBg7BANg.; 1P_JAR=2021-03-29-09; NID=212=i268DCPkYi3AzR0f25yIGeJwDvI9KnX0IkpB6-jLiMgIkylu-ok0FxsNwgb77pnNf9P1dRbBa0rwmwoo3rBZLPEqBaYbIUTYOqnGXlodQyFP6PiO7x1DARyLyIg2nH_J_J208rXWq1sLL7oP_YSeJFznofwfpsHamypEYMgwPx2rU9UJJ59txYOFOliHngVgrmyLeujCj_dKNV8hrTJDFTTVfnxZG68C; __Host-GAPS=1:BWYU84SbcmvuPTxMnLb_Bw1WhSze11euoEbasRquyke84p3z6kKhM4STn2l2KqDaXmLnjmuLAu5YjxpgPYYS2MAbFJoYEA:8QsfUKnQPG8GNFFh; SIDCC=AJi4QfGikn_BfUsmrNc_AQgbrwCzKzaBTYlqHvZ_vt7pRS98qOGuitJ1M1_khzvPELS_owtDIQ; __Secure-3PSIDCC=AJi4QfH3OD5jfNAacCFyT0_heunei0GLdQymhUmRU8zPB7R7Svse8_GiuWLuXbaSblXAYlq-7bU',
-		'google-accounts-xsrf': '1',
-		'origin': 'https://accounts.google.com',
-		'referer': 'https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fadssettings.google.com%2Fauthenticated%3Fref%3Dyt_auth%26pli%3D1&ec=GAlAmQM&flowName=GlifWebSignIn&flowEntry=AddSession',
-		'sec-ch-ua': '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
-		'sec-ch-ua-mobile': '?0',
-		'sec-fetch-dest': 'empty',
-		'sec-fetch-mode': 'cors',
-		'sec-fetch-site': 'same-origin',
-		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
-		'x-chrome-id-consistency-request': 'version=1,client_id=77185425430.apps.googleusercontent.com,device_id=eaa94017-90e6-4761-a779-143e63ce180a,sync_account_id=116486990055578668701,signin_mode=all_accounts,signout_mode=show_confirmation',
-		'x-client-data': 'CJa2yQEIo7bJAQjEtskBCKmdygEIlqzKAQiIucoBCIbCygEI+MfKAQjx6soBCLGaywEI1JzLAQjjnMsBCKmdywEY4ZrLAQ==',
-		'Decoded':
-		'message ClientVariations {// Active client experiment variation IDs. repeated int32 variation_id = [3300118, 3300131, 3300164, 3313321, 3315222, 3316872, 3318022, 3318776, 3323249, 3329329, 3329620, 3329635, 3329705]; // Active client experiment variation IDs that trigger server-side behavior. repeated int32 trigger_variation_id = [3329377];}',
-		'x-same-domain': '1' }
-    foeml=gmail
-    data = {
-		'continue': 'https://adssettings.google.com/authenticated?ref=yt_auth&pli=1',
-		'f.req': f'["{foeml}","AEThLlw6VGsyn2_pe-f86vgMSUv6HW6cl7s3ftZAG2KM59m7HtlMteSbiMd1cXodMV9ao2r-etLvEFwNOT1gmDeaWFo3pqVs2c8soJREt0Nt6O_RSxudYIUsivz-edV1f8qX-zBOsY7YMWaow-mczgAPFsOkLBibmlmgwm2DK_Mq4qKUpR1tG4YpAzApopzlBEgFAymMMus8",[],null,"US",null,null,2,false,true,[null,null,[2,1,null,1,"https://accounts.google.com/AddSession?continue=https%3A%2F%2Fadssettings.google.com%2Fauthenticated%3Fref%3Dyt_auth%26pli%3D1&ec=GAlAmQM",null,[],4,[],"GlifWebSignIn",null,[]],10,[null,null,[],null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,[5,"77185425430.apps.googleusercontent.com",["https://www.google.com/accounts/OAuthLogin"],null,null,"eaa94017-90e6-4761-a779-143e63ce180a",null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,5,null,null,[],null,null,null,[],[]],null,null,null,null,null,null,[],null,null,null,[],[]],null,null,null,true],"{foeml}",null,null,null,true,true,[]]',
-		'bgRequest': '["identifier","!XV6lXhPNAAY7smA8O7JCzxWNYzZtiS87ACkAIwj8RuOTLVLxGJigVpDhaC2z7ynSG4_C6vg2kxXAWOp26z7eH2IZogIAAAB9UgAAAB5oAQeZBBMPBNZpIQ75l0BgkmrzBFCvhfOdp8Lin14ikTnA1WsoaIfdlfYG6MYQnjc1UrPRJa74ZxDpu1BFftPI0nN7EOnM4ZS18dsZejZRwZBLtKG8QP0Q-GJEOZbq2-r2fqHe3GZb5Z7NW4rUeCLKfwsV7Tb8fuzfYlpZ_40Mw5S4Gk1C7BL4MOhtd3GE_62Qp-RuqLoqIvv5XIqMbLq6v0PjY6cUg5wg3cN4n3dF26kVWJYtS8vpVd5kBtydVCgidSNhN4ARi9Lk5TocNbPYonI5sm-7sJ0UGHI7wT4OFXJeKK_FgVxjXWI2FpFSrEPhgZahdfMB94LMNK-8eMIJKLTR-eOtu6l6TB3KVZEthoN_M0pvQHPcfAhLb0EB5tfkrcdZiMhOCGIZuCBmjzkog2_DV0Mw8Im5hMuTm8qrmX7i-lzWEmuuuYW5FGKDUraHZ0A0EmvWRaw7uMII2ScdpcXWUI6JGEjZYgGmOoTgMWVmUU0HTKuwLYkJvYxV89EdOjdtEjqfnlEl9WC21RfSxIt3mMkr9orJdP4opSk6neVuFTvvRD53NZ9iNboauX0Epe0IFa6NvidBSHLXIiaC92VUeNE08EaGEMXcy45ffg2BYsxmyu4DWRIoPcr0t1IrWdhwc1srOguxWl0mBs6QYeyLBc4CNRVglRFm3nklWlIE31g8GahYNLQnAxeljgp_WKEXda5VQm6WkDGcYGPbRf3bfxYqbJircIPhcOEYXzk8BQ9gJq9i3t3zX3qFg9U9B3skBRK4JgGKwzVWGjrw6K5G9uIOFsmXdPq7pdQE_tQ-PMPkuUPNoIZb98lu6reUbBEDHWfnpaiY6tjtTg0fcQW0kzrUN5kwJz7L47KpDYij4K3Y-hWN4vXJAFrbAHazlYZ4THHYrQDxeITF0MP6vweiyoful6H_YArrwliPX_7kswLE1MYOR_i5KwYlJEi9dMz1nSzyRynLSthBsez-zQYhlbt5Xhi8NuL3dRMxM8zRj2yw71sYdKjsX_AvNz0JX0v6W6eQSv6pVwyNAscdFsBcNt2N3xvwy0YUiFwIud5ypZe_MeWd8aByk4cce21tpZN8FDa4j6b2CGXPVpaVC7IZjCh0h_cXfFPoNONzD9roDpmOKzCrTj_q6xNPhtwY8vWdTV0CaJJbcm_ra3rp3FF0x6Hws5KZl9KpeRanphhtuiT90e-49V2AYdmbdlXgtZqJBswKwOOZhYUcw_33Q8t27ZXtiKxW2ieUv3fwvJ9Dz8l9pttsavtYUZ_mX6mN-PU4orT89JgeUDqimD91YI6Yx7KyoYlRbe7hYWNMzo0RAuuoElDr6GyTgf7pZC67XYQKm3-J3Obuja9iac7IeN7KaScI7uGcFV9gNhvP8j6evm9zws5Dlw"]',
-		'at': 'AFoagUW_q8bXR45ZgAOS4A5fq9dVpQpMBQ:1617008567951',
-		'azt': 'AFoagUW9q50Z3AnyfPsppwG7DjGdRvSUMQ:1617008567952',
-		'cookiesDisabled': 'false',
-		'deviceinfo': '[null,null,null,[],null,"US",null,null,[],"GlifWebSignIn",null,[null,null,[],null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,[5,"77185425430.apps.googleusercontent.com",["https://www.google.com/accounts/OAuthLogin"],null,null,"eaa94017-90e6-4761-a779-143e63ce180a",null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,5,null,null,[],null,null,null,[],[]],null,null,null,null,null,null,[],null,null,null,[],[]],null,null,null,null,2,null,false]',
-		'gmscoreversion': 'undefined',
-		'checkConnection': 'youtube:142:0',
-		'checkedDomains': 'youtube',
-		'pstMsg': '1'}
+from flask import *
+from requests import post,get
+def dd(email,der):
+	user = email
+	try:
+		url = f'https://www.instagram.com/api/v1/users/web_profile_info/?username={user}'
+		headers = {
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'ar-AE,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Cookie': 'mid=ZHTlnQALAAFHZAE8G64BeLHXNMv6; ig_did=ACB29C06-4F89-4B7A-9D37-DC433D1E9398; ig_nrcb=1; datr=nUZ1ZAphhPG3siVLQu3QFbkq; fbm_124024574287414=base_domain=.instagram.com; csrftoken=1gI1BSItuCt7GpIB7BL3KCrapTfKligx; ds_user_id=55002803434; sessionid=55002803434%3A1m1laRSPbJaoKD%3A24%3AAYdWXJwfQhhN68tU0NIkcNODEtrIYnAgKCWPkrp3Rg; shbid="12254\05455002803434\0541717693792:01f7d20c44658c09775e0f963159681bf19a10be70bbe95b497a89f112ac2fc01ab50da0"; shbts="1686157792\05455002803434\0541717693792:01f7c175c7d720e51402db9b91b351fab16619ea5a91f0ce421bc4c9827bce14e62426c5"; fbsr_124024574287414=Z3GOftVJ7wWK4lDsT4vYGKDlPKHv5vYXWQpT8AYi130.eyJ1c2VyX2lkIjoiMTAwMDg3NjM5MTE3ODM3IiwiY29kZSI6IkFRRHRIbWwtakhjY25sQmxidDJmcGpDZmtLV2stb2FQY0lLbHpWQWtfMUlhLTNqMF9wdlhsaFUyTnJvYXRsT2lvUmJfSXNzc19oSXFyYzFRX3BLZ1RaX1RSTEhCbGpzRTFHZkFjWWJoX0Q4aVVwYWdSR2Q0bVNXcUVwai1SajlkT1J5RmxadzZHbWZCc0ZCbVdUY0RDNDAzUFVnTzV2TVBONk1UcmpSUDlpTU85dFdSc0hURFdsUVhrNDJycVhvbzM2SHlnYXRNdDJMRWlNNUZrcmVfRWtiWGUzTTlqdzY4enpKT2RVUjlIUmt2TUlXcWZqQ2RUc3FmYUo5MWowUF95bm9aLVZCSnpmb0xuSkt3MV9JTkFTQzdEZmM3ZURIeDFiTkFyRS1SQ1FhYUp3ZWtydVdzMFBYaV9pTDdYTlZrRTg5Yy1oYWVrWVI1YU45cDhwVXp0ZXBsIiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCQUhiWkNnM3M0UXJRZmhyOXRaQm5mdHI2cTlsYXJNMnpRaFpDZTNlczJOTGkwNGc4NGJaQldRTWs4VlpDWkNZMVZ3ak52azJ4M0d0VkxaQTdPajhZWkFkNklDdUxtMjI0NllhVTZQdXRlMk1PU3haQnpxbDhxUnU2UDhRYTZnRXhpUGRRbHB5WWJYSmVRczB3N2UzdFRxZXdnQWdUYXNpYzRjd0d3MHpaQUxTdXNCVUN3Y0JnVnk3MmdaRCIsImFsZ29yaXRobSI6IkhNQUMtU0hBMjU2IiwiaXNzdWVkX2F0IjoxNjg2MjA3NDQ4fQ; rur="ODN\05455002803434\0541717743459:01f7dc2b656c6f698ae45a64240745cb3e01e62cb90350349fcf91dba76a5d92e481be60"',
+    'Referer': 'https://www.instagram.com/5u2.a/',
+    'Sec-Ch-Prefers-Color-Scheme': 'dark',
+    'Sec-Ch-Ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+    'Sec-Ch-Ua-Full-Version-List': '"Not.A/Brand";v="8.0.0.0", "Chromium";v="114.0.5735.110", "Google Chrome";v="114.0.5735.110"',
+    'Sec-Ch-Ua-Mobile': '?0',
+    'Sec-Ch-Ua-Platform': '"Windows"',
+    'Sec-Ch-Ua-Platform-Version': '"10.0.0"',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+    'Viewport-Width': '624',
+    'X-Asbd-Id': '129477',
+    'X-Csrftoken': '1gI1BSItuCt7GpIB7BL3KCrapTfKligx',
+    'X-Ig-App-Id': '936619743392459',
+    'X-Ig-Www-Claim': 'hmac.AR2f295htsHXPFyt6RxGipeoIQHM6Vikj5SuhBSAT7RgrCSq',
+    'X-Requested-With': 'XMLHttpRequest',}
+		rr = get(url,headers=headers).json()
+		url='https://i.instagram.com/api/v1/accounts/send_recovery_flow_email/'
+		haha={
+ 'X-Pigeon-Session-Id':'2b712457-ffad-4dba-9241-29ea2f472ac5',
+ 'X-Pigeon-Rawclienttime':'1707104597.347',
+ 'X-IG-Connection-Speed':'-1kbps',
+ 'X-IG-Bandwidth-Speed-KBPS':'-1.000',
+ 'X-IG-Bandwidth-TotalBytes-B':'0',
+ 'X-IG-Bandwidth-TotalTime-MS':'0',
+ 'X-IG-VP9-Capable':'false',
+ 'X-Bloks-Version-Id':'009f03b18280bb343b0862d663f31ac80c5fb30dfae9e273e43c63f13a9f31c0',
+ 'X-IG-Connection-Type':'WIFI',
+ 'X-IG-Capabilities':'3brTvw==',
+ 'X-IG-App-ID':'567067343352427',
+ 'User-Agent':'Instagram 100.0.0.17.129 Android (30/11; 320dpi; 720x1448; realme; RMX3231; RMX3231; RMX3231; ar_IQ; 161478664)',
+ 'Accept-Language':'ar-IQ, en-US',
+ 'Cookie':'mid=Zbu4xQABAAE0k2Ok6rVxXpTD8PFQ; csrftoken=dG4dEIkWvAWpIj1B2M2mutWtdO1LiPCK',
+ 'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+ 'Accept-Encoding':'gzip, deflate',
+ 'Host':'i.instagram.com',
+ 'X-FB-HTTP-Engine':'Liger',
+ 'Connection':'keep-alive',
+ 'Content-Length':'364',
+ }
+		ada={
+ 'signed_body':'ef02f559b04e8d7cbe15fb8cf18e2b48fb686dafd056b7c9298c08f3e2007d43.{"_csrftoken":"dG4dEIkWvAWpIj1B2M2mutWtdO1LiPCK","adid":"5e7df201-a1ff-45ec-8107-31b10944e25c","guid":"b0382b46-1663-43a7-ba90-3949c43fd808","device_id":"android-71a5d65f74b8fcbc","query":"'f'{user}''"}',
+ 
+ 'ig_sig_key_version':'4',
+ }
+		s=post(url,headers=haha,data=ada).text
+		url = 'http://www.bradychrist.top/api/v7/user'
+		he = {
+'Host': 'www.bradychrist.top',
+'Connection': 'keep-alive',
+'Content-Length': '13',
+'package': 'ins.bradychrist.com',
+'apptype': 'android',
+'User-Agent': 'Mozilla/5.0 (Linux; Android 13; ANY-LX2 Build/HONORANY-L22CQ; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/120.0.6099.211 Mobile Safari/537.36',
+ 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+  'idfa': '93f87de7-a83b-4098-a8a7-6801539c5f6a',
+  'Accept': 'application/json, text/plain, */*',
+  'pk': '',
+  'username': '',
+  'version': '1.0',
+  'Origin': 'http://www.bradychrist.top',
+  'X-Requested-With': 'ins.bradychrist.com',
+  'Referer': 'http://www.bradychrist.top/h5_v12/',
+  'Accept-Encoding': 'gzip, deflate',
+  'Accept-Language': 'ar-IQ,ar;q=0.9,en-IQ;q=0.8,en-US;q=0.7,en;q=0.6',
+} 
+		da=(f'username=ks_.j')
+		zaid=post(url,headers=he,data=da).json()
+		re=get(f'https://o7aa.pythonanywhere.com/?id=44787770512').json()
+		datet=re['date']
+		avatar=zaid['data']['avatar']
+		id1=zaid['data']['userPk']
+		pos=zaid['data']['postsNum']
+		name = rr['data']['user']['full_name']
+		bio = rr['data']['user']['biography']
+		fol = rr['data']['user']['edge_followed_by']['count']
+		fols = rr['data']['user']['edge_follow']['count']
+		id = rr['data']['user']['id']
+		if '"تم إرسال البريد الإلكتروني"' in s:
+			try:
+				rest = s.split('"email":"')[1].split('","status":"')[0]
+			except:
+				rest = 'Error'
+		return {'name': name, 'user': user, 'email': f'{user}@gmail.com','data': datet, 'follower': fol, 'folloers': fols,'posts': pos,'id': id, 'bio': bio,'image': avatar,'rest': rest, 'dev': 'https://t.me/d_xiim'}
+	except:
+		return {'error?': f'error/get/info/user?={user}', 'dev': 'https://t.me/d_xiim'}
 
-    ok = r.post(url,headers=headers,data=data).text
-    if "https://support.google.com/accounts/answer" in ok:
-    		
-    		data2={
-    		"email":f"{gmail}",
-    		"status":"Bad",
-    		"Dev":"Marko_Bots",
-    		}
-    		return JSONResponse(content=data2)
-    else:
-    		dta={
-    		"email":f"{gmail}",
-    		"status":"Hit",
-    		"Dev":"Marko_Bots",
-    		}
-    		return JSONResponse(content=dta)
+app = Flask(__name__)
+@app.route('/<num>/user=<text>')
 
-#uvicorn.run(app,host='0.0.0.0',port=8080)
+def de(text,num):
+	try:
+		return {'done check': dd(text,int(num))}
+	except:
+		return {'bad': 'user not fond'}
+		
+		
+if __name__ == '__main__':
+	app.run(debug=True)
